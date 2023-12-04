974aa3783672685b2035fb640a520f2dd47f48fc
PaulYH <paulhumenny@icloud.com>
2023-12-04 11:45:43 -0500

    Add account details page

--- Concordia For Students/app/src/main/java/com/example/concordiaforstudents/meFragments/AccountDetailsFragment.kt
+++ Concordia For Students/app/src/main/java/com/example/concordiaforstudents/meFragments/AccountDetailsFragment.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import com.example.concordiaforstudents.R
 
@@ -14,7 +16,18 @@ class AccountDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         Log.d("AccountDetailsFragment", "onCreateView called")
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_accountdetails, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_accountdetails, container, false)
+
+        val text: TextView = view.findViewById(R.id.mainTextView)
+
+        val inputText = "<b>Name:</b><br/> John Doe <br/><br/>" +
+                        "<b>Student ID:</b><br/> 40254021<br/><br/>" +
+                        "<b>Address:</b><br/> 1234 Avenue Street, Montreal, QC<br/><br/>" +
+                        "<b>Primary Phone Number:</b><br/> (514) 248-2934<br/><br/>" +
+                        "<b>Email:</b><br/> john_doe@gmail.com<br/><br/>"
+        text.text = HtmlCompat.fromHtml(inputText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+        return view
     }
 }
